37a367a7e8c7c819cb5b6e2f5a107a951238fbce
Daniela <daniela.orellana.c@usach.cl>
2023-06-17 17:23:57 -0400

    [Daniela Orellana] arreglo pag de emergencias

--- pages/emergencias.vue
+++ pages/emergencias.vue
@@ -1,23 +1,24 @@
 <template>
-  <div>
-    <h1>Listado de Emergencias</h1>
-    <ul>
-      <li v-for="emergencia in emergencias" :key="emergencia.id_emergencia">
-        <h3>{{ emergencia.asunto }}</h3>
-        <p>{{ emergencia.descripcion }}</p>
-        <p>{{ emergencia.direccion }}</p>
-        <p>{{ emergencia.fecha }}</p>
-        <p>{{ emergencia.activa }}</p>
-        <p>{{ emergencia.id_institucion }}</p>
-        <p>{{ emergencia.region }}</p>
-        <p>{{ emergencia.ubicacion }}</p>
-      </li>
-    </ul>
-  </div>
+  <v-container class="emergencias-container">
+    <h1 class="page-title">Listado de Emergencias</h1>
+    <v-list>
+      <v-list-item v-for="emergencia in emergencias" :key="emergencia.id_emergencia" class="emergencia-item">
+        <v-list-item-content>
+          <v-list-item-title class="asunto">{{ emergencia.asunto }}</v-list-item-title>
+          <v-list-item-subtitle class="descripcion">{{ emergencia.descripcion }}</v-list-item-subtitle>
+          <v-list-item-subtitle class="direccion">{{ emergencia.direccion }}</v-list-item-subtitle>
+          <v-list-item-subtitle class="fecha">{{ emergencia.fecha }}</v-list-item-subtitle>
+          <v-list-item-subtitle class="activa">{{ emergencia.activa }}</v-list-item-subtitle>
+          <v-list-item-subtitle class="institucion">{{ emergencia.id_institucion }}</v-list-item-subtitle>
+          <v-list-item-subtitle class="region">{{ emergencia.region }}</v-list-item-subtitle>
+          <v-list-item-subtitle class="ubicacion">{{ emergencia.ubicacion }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-container>
 </template>
 
 <script>
-import axios from 'axios';
 import EmergenciaServices from "~/services/EmergenciaServices";
 
 export default {
@@ -38,3 +39,44 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.emergencias-container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.page-title {
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
+}
+
+.emergencia-item {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+.asunto {
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.descripcion {
+  font-size: 14px;
+  margin-bottom: 5px;
+}
+
+.direccion,
+.fecha,
+.activa,
+.institucion,
+.region,
+.ubicacion {
+  font-size: 12px;
+  margin-bottom: 3px;
+}
+</style>
--- services/EmergenciaServices.js
+++ services/EmergenciaServices.js
@@ -1,8 +1,11 @@
 import Api from "~/services/Api";
+import axios from "axios";
 
 export default {
   findAll(token){
     console.log("token: "+token);
+    // Configurar el token en los encabezados de Axios
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
     return Api().get('/api/emergencias');
   },
 
